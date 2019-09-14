52691b05536db14bf8ded1213372bac3df2661bc
Alex Kravtsov <alexandr.kravtsov.ua@gmail.com>
2019-09-13 22:45:48 -0700

    Step2

--- StringCalculatorBLLTests/CalculatorTests.cs
+++ StringCalculatorBLLTests/CalculatorTests.cs
@@ -5,6 +5,12 @@ namespace StringCalculatorBLLTests
 {
     public class CalculatorTests
     {
+        private readonly Calculator _calculator = new Calculator();
+        public CalculatorTests()
+        {
+            
+        }
+
         [InlineData("20", 20)]
         [InlineData("-20", -20)]
         [InlineData("0,-5", -5)]
@@ -15,16 +21,16 @@ namespace StringCalculatorBLLTests
         [Theory]
         public void SimplePositiveAddTest(string input, long expectedResult)
         {
-            //Arrange
-            var calculator = new Calculator();
-
             //Act
-            var actualResult = calculator.Add(input);
+            var actualResult = _calculator.Add(input);
 
+            //Assert
             Assert.Equal(expectedResult, actualResult);
         }
 
         [InlineData("", 0)]
+        [InlineData(" ", 0)]
+        [InlineData(null, 0)]
         [InlineData(",", 0)]
         [InlineData(",20", 20)]
         [InlineData("10,", 10)]
@@ -36,13 +42,46 @@ namespace StringCalculatorBLLTests
         [Theory]
         public void SimpleNegativeAddTest(string input, long expectedResult)
         {
-            //Arrange
-            var calculator = new Calculator();
-
             //Act
-            var actualResult = calculator.Add(input);
+            var actualResult = _calculator.Add(input);
 
+            //Assert
             Assert.Equal(expectedResult, actualResult);
         }
+
+
+        [InlineData("20,30,50", 100)]
+        [InlineData("1,2,3,4,5,6,7,8,9,10,11,12", 78)]
+        [InlineData("-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12", -78)]
+        [InlineData("5000000000,5000000000,5000000000,5000000000", 20000000000L)]
+        [Theory]
+        public void MultiplePositiveAddTest(string input, long expectedResult)
+        {
+            //Act
+            var actualResult = _calculator.Add(input);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+
+        [InlineData(",,,,,,", 0)]
+        [InlineData(",,,20", 20)]
+        [InlineData("10,,,,", 10)]
+        [InlineData(",,,50,,,", 50)]
+        [InlineData("one,two,qwe,rty", 0)]
+        [InlineData("a10,20,c30", 20)]
+        [InlineData("10,20a,z30", 10)]
+        [InlineData("01b,01b,11b", 0)]
+        [Theory]
+        public void MultipleNegativeAddTest(string input, long expectedResult)
+        {
+            //Act
+            var actualResult = _calculator.Add(input);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
     }
 }
