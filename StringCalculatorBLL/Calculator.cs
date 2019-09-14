934489b0eff1438f76c477e49aff667256485ed0
Alex Kravtsov <alexandr.kravtsov.ua@gmail.com>
2019-09-13 22:03:38 -0700

    Step1

--- StringCalculatorBLL/Calculator.cs
+++ StringCalculatorBLL/Calculator.cs
@@ -1,11 +1,20 @@
-﻿namespace StringCalculatorBLL
+﻿using System;
+
+namespace StringCalculatorBLL
 {
     public class Calculator
     {
         public long Add(string input)
         {
-            var numbers = input.Split(',');
+            var numbers = input.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
             long sum = 0;
+            foreach (var s in numbers)
+            {
+                if (long.TryParse(s, out var number))
+                {
+                    sum += number;
+                }
+            }
 
             return sum;
         }
--- StringCalculatorBLLTests/CalculatorTests.cs
+++ StringCalculatorBLLTests/CalculatorTests.cs
@@ -11,9 +11,30 @@ namespace StringCalculatorBLLTests
         [InlineData("0,0", 0)]
         [InlineData("-50,-100", -150)]
         [InlineData("1,5000", 5001)]
-        [InlineData("5000000000,5000000000", 100000000000L)]
+        [InlineData("5000000000,5000000000", 10000000000L)]
         [Theory]
-        public void SimpleAddTest(string input, long expectedResult)
+        public void SimplePositiveAddTest(string input, long expectedResult)
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var actualResult = calculator.Add(input);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [InlineData("", 0)]
+        [InlineData(",", 0)]
+        [InlineData(",20", 20)]
+        [InlineData("10,", 10)]
+        [InlineData("one", 0)]
+        [InlineData("one,two", 0)]
+        [InlineData("a10,20", 20)]
+        [InlineData("10,20a", 10)]
+        [InlineData("01b,01b", 0)]
+        [Theory]
+        public void SimpleNegativeAddTest(string input, long expectedResult)
         {
             //Arrange
             var calculator = new Calculator();
